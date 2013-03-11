53cbd37f16ba4fb2f8ba24b4f750bbdc99fb31c6
peteraritchie <github.com@peterritchie.com>
2013-03-11 13:04:34 -0400

    added more tests
    changed tests to work with instance of IoC

--- Tests/when_getting_types_from_container.cs
+++ Tests/when_getting_types_from_container.cs
@@ -6,40 +6,47 @@ using Xunit;
 
 namespace Tests
 {
+// ReSharper disable InconsistentNaming
 	public class when_getting_types_from_container
 	{
+		private IoC ioc;
+		public when_getting_types_from_container()
+		{
+			ioc = new IoC();
+		}
+
 		[Fact]
 		public void then_resolving_type_from_unloaded_assembly_works()
 		{
-			var x = IoC.Resolve<IFormattable>();
+			var x = ioc.Resolve<IFormattable>();
 			Assert.Equal("TestTypes.Class1", x.GetType().FullName);
 		}
 
 		[Fact]
 		public void then_resolving_instance_of_mapped_interface_correct_instance_results()
 		{
-			var instance = IoC.Resolve<IInterface>();
+			var instance = ioc.Resolve<IInterface>();
 			Assert.IsType<InterfaceImplementation>(instance);
 		}
 
 		[Fact]
 		public void then_resolving_instance_of_unmapped_type_results_with_null()
 		{
-			var instance = IoC.Resolve<System.Collections.Generic.ISet<int>>();
+			var instance = ioc.Resolve<ISet<int>>();
 			Assert.Null(instance);
 		}
 
 		[Fact]
 		public void then_resolving_concrete_type_with_default_constructor_instance_of_type_results()
 		{
-			var instance = IoC.Resolve<ArgumentException>();
+			var instance = ioc.Resolve<ArgumentException>();
 			Assert.IsType<ArgumentException>(instance);
 		}
 
 		[Fact]
 		public void then_resolving_concrete_dependency_correct_instance_results()
 		{
-			var instance = IoC.Resolve<DependantTwo>();
+			var instance = ioc.Resolve<DependantTwo>();
 			Assert.NotNull(instance);
 			Assert.IsType<DependantTwo>(instance);
 		}
@@ -47,7 +54,7 @@ namespace Tests
 		[Fact]
 		public void then_resolving_concrete_hierarchial_dependency_correct_instance_results()
 		{
-			var instance = IoC.Resolve<DependantOne>();
+			var instance = ioc.Resolve<DependantOne>();
 			Assert.NotNull(instance);
 			Assert.IsType<DependantOne>(instance);
 		}
@@ -55,14 +62,14 @@ namespace Tests
 		[Fact]
 		public void then_resolving_generic_concrete_type_correct_instance_type_results()
 		{
-			var instance = IoC.Resolve<List<int>>();
+			var instance = ioc.Resolve<List<int>>();
 			Assert.NotNull(instance);
 		}
 
 		[Fact]
 		public void then_resolving_open_generic_correct_type_results()
 		{
-			IEnumerable<int> instance = IoC.Resolve<IEnumerable<int>>();
+			IEnumerable<int> instance = ioc.Resolve<IEnumerable<int>>();
 			Assert.NotNull(instance);
 			Assert.IsType<List<int>>(instance);
 		}
@@ -70,7 +77,7 @@ namespace Tests
 		[Fact]
 		public void then_resolving_generic_results_in_correct_type()
 		{
-			var instance = IoC.Resolve<ICollection<int>>();
+			var instance = ioc.Resolve<ICollection<int>>();
 			Assert.NotNull(instance);
 			Assert.IsType<List<int>>(instance);
 		}
@@ -78,27 +85,65 @@ namespace Tests
 		[Fact]
 		public void then_resolving_closed_generic_with_open_generic_registration_succeeds()
 		{
-			IoC.RegisterType(typeof(IList<>), typeof(List<>));
-			IoC.Resolve<IList<int>>();
+			ioc.RegisterType(typeof(IList<>), typeof(List<>));
+			ioc.Resolve<IList<int>>();
 		}
 
 		[Fact]
 		public void then_resolving_closed_generic_with_incompatible_closed_generic_registration_fails()
 		{
-			IoC.RegisterType(typeof(IGeneric<>), typeof(GenericImplementation<string>));
-			Assert.Throws<InvalidCastException>(() => IoC.Resolve<IGeneric<int>>());
+			ioc.RegisterType(typeof(IGeneric<>), typeof(GenericImplementation<string>));
+			Assert.Throws<InvalidCastException>(() => ioc.Resolve<IGeneric<int>>());
 		}
 
 		[Fact]
 		public void then_registering_incompatible_open_generics_fails()
 		{
-			Assert.Throws<InvalidOperationException>(()=>IoC.RegisterType(typeof(IList<>), typeof(HashSet<>)));
+			Assert.Throws<InvalidOperationException>(()=>ioc.RegisterType(typeof(IList<>), typeof(HashSet<>)));
 		}
 
 		[Fact]
 		public void then_resolving_circular_dependency_causes_exception()
 		{
-			Assert.Throws<CircularDependencyException>(() => IoC.Resolve<CircularDependant2>());
+			Assert.Throws<CircularDependencyException>(() => ioc.Resolve<CircularDependant2>());
+		}
+
+		[Fact]
+		public void then_resolving_delegate_mapping_results_in_success()
+		{
+			ioc.RegisterTypeFunc(typeof(DelegateBase), type => new DelegateSuper());
+			Assert.IsType<DelegateSuper>(ioc.Resolve<DelegateBase>());
+		}
+
+		[Fact]
+		private void then_resolving_named_instance_succeeds()
+		{
+			var expected = new[] {1, 2, 3, 4};
+			ioc.RegisterInstance(expected, "myArray");
+			Assert.Equal(expected, ioc.Resolve<int[]>("myArray"));
+		}
+
+		[Fact]
+		private void then_resolving_named_instance_in_config_succeeds()
+		{
+			var expected = new List<int>();
+			Assert.Equal(expected, ioc.Resolve<List<int>>("myIntList"));
+		}
+
+		[Fact]
+		private void then_resolving_unnamed_type_results_in_null()
+		{
+			var actual = ioc.Resolve<IList<int>>("gibberish");
+			Assert.Null(actual);
+		}
+
+		[Fact]
+		private void then_resolving_named_type_succeeds()
+		{
+			ioc.RegisterType<IList<int>, List<int>>("myListType");
+			var actual = ioc.Resolve<IList<int>>("myListType");
+			Assert.IsType<List<int>>(actual);
 		}
 	}
+	// ReSharper restore InconsistentNaming
 }
